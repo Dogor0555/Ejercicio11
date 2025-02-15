a6c49daf4f85d11e81765612d16830621229dcc9
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 06:09:43 -0600

    Ejercicio11

--- Ejercicio11/Program.cs
+++ Ejercicio11/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+Console.Write("Ingrese el primer número: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el segundo número: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.Write("Ingrese el tercer número: ");
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 >= num2 && num1 >= num3)
+{
+    Console.WriteLine("El número mayor es: " + num1);
+}
+else if (num2 >= num1 && num2 >= num3)
+{
+    Console.WriteLine("El número mayor es: " + num2);
+}
+else
+{
+    Console.WriteLine("El número mayor es: " + num3);
+}
